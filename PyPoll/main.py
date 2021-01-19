fe4cd7f5c87bd36115e1589b3b29b4271a98da55
Riley Fritz <rileyfritz5773@gmail.com>
2021-01-19 16:20:15 -0600

    PyPoll working, need to add output to txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,7 +14,11 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 
 # Variables 
 vote_count = 0
+most_votes = 0
 candidate_list = []
+candidate_votes_dict = {}
+all_votes = []
+
 
 with open(csvpath) as csvfile:
 
@@ -34,7 +38,29 @@ with open(csvpath) as csvfile:
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
 
+        # Put all votes in a list
+        all_votes.append(row[2])
+    
+    # Print vote count
+    print(f'There were {vote_count} votes during this election!')
 
+    # create dictionary with each candidate and vote count
+    for candidate in candidate_list:
+        votes_recieved = all_votes.count(candidate)
+        candidate_votes_dict[candidate] = all_votes.count(candidate)
+        # Looking for the winner
+        if votes_recieved > most_votes:
+            most_votes = votes_recieved
+            winning_candidate = candidate
+        # print out results to terminal
+        print(f'{candidate} received {votes_recieved} votes ({round((votes_recieved / vote_count) * 100, 0)}%)')
+        
+    # Print winner
+    print(f'{winning_candidate} is the winner!')
+    
+with open(output_path, 'w') as txtfile:
+    for candidate, votes in candidate_votes_dict.items():
+        txtfile.write(f'{candidate} recieved {votes} votes')
+        writer.write('\n')
 
-print(vote_count)
 
